f16490591c87b872dea34cace6169b98f56ba9bf
TrailRunner65 <jwtempleton65@gmail.com>
2020-05-11 21:42:04 +1000

    Initial commit

---
+++ src/main/java/au/com/abstractcs/runcalcs/RuncalcsApplication.java
@@ -0,0 +1,13 @@
+package au.com.abstractcs.runcalcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuncalcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuncalcsApplication.class, args);
+	}
+
+}
